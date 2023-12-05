6ec4bbf4a6004f68966efca8b501d4ae33a07932
Nikol <482269@student.fontys.nl>
2023-12-05 15:09:12 +0100

    token converter bug fixed

--- src/main/java/com/example/pawsicare/business/impl/TokenConverter.java
+++ src/main/java/com/example/pawsicare/business/impl/TokenConverter.java
@@ -2,16 +2,26 @@ package com.example.pawsicare.business.impl;
 
 import com.example.pawsicare.business.security.token.impl.AccessTokenImpl;
 import com.example.pawsicare.domain.RefreshToken;
+import com.example.pawsicare.domain.User;
 import com.example.pawsicare.persistence.UserEntityConverter;
+import com.example.pawsicare.persistence.entity.UserEntity;
 import com.example.pawsicare.persistence.jparepositories.UserRepository;
+
+import java.util.Optional;
 
 public class TokenConverter {
 
     UserRepository userRepository;
     UserEntityConverter userEntityConverter;
     public RefreshToken toRefreshToken (AccessTokenImpl token){
+        Optional<UserEntity> userEntity = userRepository.getUserEntityById(token.getId());
+        User user = null;
+
+        if(userEntity.isPresent()){
+            user = userEntityConverter.fromUserEntity(userEntity.get());
+        }
         return RefreshToken.builder()
-                .userInfo(userEntityConverter.fromUserEntity(userRepository.getUserEntityById(token.getId()).get()))
+                .userInfo(user)
                 .token(token.toString())
                 .expiryDate(token.getExpiration())
                 .build();
